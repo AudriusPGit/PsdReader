6593ceb358fa9a6228aed9cbef0430271201a48f
AudriusPGit <AudriusGit@gmail.com>
2015-08-30 01:17:19 -0700

    Project setup

---
+++ PsdReader/PsdReader/IvAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IvAppDelegate.h
+//  PsdReader
+//
+//  Created by Brolis on 8/30/15.
+//  Copyright (c) 2015 Iv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IvAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PsdReader/PsdReader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PsdReader
+//
+//  Created by Brolis on 8/30/15.
+//  Copyright (c) 2015 Iv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IvAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IvAppDelegate class]));
+    }
+}
